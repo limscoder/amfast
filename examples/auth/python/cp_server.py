88d2feb57a623a10f869e7bce0259160412b2afb
dthomp325 <dthomp325@e836f868-076b-11de-b55b-d397e7115e68>
2009-05-02 00:33:32 +0000

    Started Auth example.

---
+++ examples/auth/python/cp_server.py
@@ -0,0 +1,54 @@
+"""An example server using the CherryPy web framework."""
+import os
+import optparse
+
+import cherrypy
+
+import amfast
+from amfast.remoting.channel import ChannelSet
+from amfast.remoting.cherrypy_channel import CherryPyChannel
+import utils
+
+class App(object):
+    """Base web app."""
+    @cherrypy.expose
+    def index(self):
+        raise cherrypy.HTTPRedirect('/hello_world.html')
+
+if __name__ == '__main__':
+    usage = """usage: %s [options]""" % __file__
+    parser = optparse.OptionParser(usage=usage)
+    parser.add_option("-p", default=8000,
+        dest="port", help="port number to serve")
+    parser.add_option("-d", default="localhost",
+        dest="domain", help="domain to serve")
+    parser.add_option("-l", action="store_true",
+        dest="log_debug", help="log debugging output")
+    (options, args) = parser.parse_args()
+
+    amfast.log_debug = options.log_debug
+
+    cp_options = {
+        'global':
+        {
+            'server.socket_port': int(options.port),
+            'server.socket_host': str(options.domain),
+        },
+        '/':
+        {
+            'tools.staticdir.on': True,
+            'tools.staticdir.dir': os.path.join(os.getcwd(), '../flex/deploy')
+        }
+    }
+
+    channel_set = ChannelSet()
+    rpc_channel = CherryPyChannel('amf-channel')
+    channel_set.mapChannel(rpc_channel)
+    utils.setup_channel_set(channel_set)
+
+    app = App()
+    app.amf = rpc_channel.processMsg
+    cherrypy.quickstart(app, '/', config=cp_options)
+
+    print "Serving on %s:%s" % (options.domain, options.port)
+    print "Press ctrl-c to halt."
---
+++ examples/auth/python/utils.py
@@ -0,0 +1,25 @@
+"""Utility functions."""
+import sys
+import logging
+
+import amfast
+from amfast.encoder import Encoder
+from amfast.decoder import Decoder
+from amfast.remoting import Service, CallableTarget
+
+import controller
+
+def setup_channel_set(channel_set):
+    """Configures an amfast.remoting.channel.ChannelSet object."""
+
+    # Send log messages to STDOUT
+    handler = logging.StreamHandler(sys.stdout)
+    handler.setLevel(logging.DEBUG)
+    amfast.logger.addHandler(handler)
+
+    # Map service targets to controller methods
+    cont_obj = controller.Controller()
+    service = Service('ExampleService')
+    service.mapTarget(CallableTarget(cont_obj.echo, 'echo'))
+    service.mapTarget(CallableTarget(cont_obj.raiseException, 'raiseException'))
+    channel_set.service_mapper.mapService(service)
