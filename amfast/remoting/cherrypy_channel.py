d5fd94f0f8d181927591ec4ef59e16d87a37d10b
dthomp325 <dthomp325@e836f868-076b-11de-b55b-d397e7115e68>
2009-09-16 22:42:26 +0000

    Fixed bug #32 in CherryPy Channel.

--- amfast/remoting/cherrypy_channel.py
+++ amfast/remoting/cherrypy_channel.py
@@ -15,7 +15,7 @@ def amfhook():
     cherrypy.request.show_tracebacks = False
 
     if cherrypy.request.method != 'POST':
-        raise cherrypy.HTTPError(405, self.getBadMethodMsg());
+        raise cherrypy.HTTPError(405, "405 Method Not Allowed\n\nAMF request must use 'POST' method.");
 cherrypy.tools.amfhook = cherrypy.Tool('before_request_body', amfhook, priority=0)
 
 class CherryPyChannelSet(ChannelSet):
